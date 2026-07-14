ca0e4b486be0b2549756c4359ee05fd779e53c39
PranavAggarwal926143 <pranav.ag1420@gmail.com>
2026-07-14 15:54:55 +0530

    spell

--- src/assets/DBentries.jsx
+++ src/assets/DBentries.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function DBEntries() {
+function DBentries() {
 
   const [entries, setEntries] = useState([]);
   const [editingId, setEditingId] = useState(null);
